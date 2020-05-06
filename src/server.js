9deba03734823fa7323312e2dc2c9a0b9e37063a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-05-06 20:27:01 -0300

    Implementação Bull Board

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 
+import BullBoard from 'bull-board';
+import Queue from './app/lib/Queue';
+
 import routes from './app/routes';
 
 const app = express();
+BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
+
+app.use('/admin/queues', BullBoard.UI);
 
 app.use(express.json());
 app.use(routes);
